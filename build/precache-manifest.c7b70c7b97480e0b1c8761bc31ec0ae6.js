7c095f816fd656abdf2e2c1208cf1e22f0bf8072
ROB <kaczurr@gmail.com>
2022-10-09 23:15:01 -0400

    adding all changes at once which includes package.json work and app src

---
+++ build/precache-manifest.c7b70c7b97480e0b1c8761bc31ec0ae6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "03265f50156714fed659",
+    "url": "/bitwiserob/comp3104_lab_f2022/static/css/main.daa30d24.chunk.css"
+  },
+  {
+    "revision": "03265f50156714fed659",
+    "url": "/bitwiserob/comp3104_lab_f2022/static/js/main.03265f50.chunk.js"
+  },
+  {
+    "revision": "59135fcce19d1f2d76f5",
+    "url": "/bitwiserob/comp3104_lab_f2022/static/js/runtime~main.59135fcc.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/bitwiserob/comp3104_lab_f2022/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "f24697f960ac969335bb60ad63b54f76",
+    "url": "/bitwiserob/comp3104_lab_f2022/index.html"
+  }
+];
